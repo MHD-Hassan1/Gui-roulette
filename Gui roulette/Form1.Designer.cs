2a61e4cc3e62dc59314a016c323ed21bbdbdb95b
MHD-Hassan1 <14MA0801@amal.se>
2026-04-15 09:43:45 +0200

    Add project files.

---
+++ Gui roulette/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Gui_roulette
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            errorProvider1 = new System.Windows.Forms.ErrorProvider(components);
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            balance = new System.Windows.Forms.Label();
+            betamount = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            betred = new System.Windows.Forms.Button();
+            betblack = new System.Windows.Forms.Button();
+            betodd = new System.Windows.Forms.Button();
+            beteven = new System.Windows.Forms.Button();
+            reset = new System.Windows.Forms.Button();
+            resultat = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            nrbtn = new System.Windows.Forms.TextBox();
+            pictureBox2 = new System.Windows.Forms.PictureBox();
+            betbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (System.Drawing.Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new System.Drawing.Point(131, 94);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(557, 266);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // balance
+            // 
+            balance.AutoSize = true;
+            balance.ForeColor = System.Drawing.Color.White;
+            balance.Location = new System.Drawing.Point(666, 31);
+            balance.Name = "balance";
+            balance.Size = new System.Drawing.Size(100, 20);
+            balance.TabIndex = 1;
+            balance.Text = "Balance: $100";
+            // 
+            // betamount
+            // 
+            betamount.Location = new System.Drawing.Point(21, 61);
+            betamount.Name = "betamount";
+            betamount.Size = new System.Drawing.Size(125, 27);
+            betamount.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(21, 31);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(126, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Enter Bet Amount";
+            // 
+            // betred
+            // 
+            betred.BackColor = System.Drawing.Color.Red;
+            betred.ForeColor = System.Drawing.Color.White;
+            betred.Location = new System.Drawing.Point(174, 366);
+            betred.Name = "betred";
+            betred.Size = new System.Drawing.Size(94, 29);
+            betred.TabIndex = 4;
+            betred.Text = "Bet Red";
+            betred.UseVisualStyleBackColor = false;
+            // 
+            // betblack
+            // 
+            betblack.BackColor = System.Drawing.Color.Black;
+            betblack.ForeColor = System.Drawing.Color.White;
+            betblack.Location = new System.Drawing.Point(174, 401);
+            betblack.Name = "betblack";
+            betblack.Size = new System.Drawing.Size(94, 29);
+            betblack.TabIndex = 5;
+            betblack.Text = "Bet Black";
+            betblack.UseVisualStyleBackColor = false;
+            // 
+            // betodd
+            // 
+            betodd.BackColor = System.Drawing.Color.Gray;
+            betodd.ForeColor = System.Drawing.Color.White;
+            betodd.Location = new System.Drawing.Point(563, 401);
+            betodd.Name = "betodd";
+            betodd.Size = new System.Drawing.Size(94, 29);
+            betodd.TabIndex = 6;
+            betodd.Text = "Bet Odd";
+            betodd.UseVisualStyleBackColor = false;
+            // 
+            // beteven
+            // 
+            beteven.BackColor = System.Drawing.Color.SandyBrown;
+            beteven.ForeColor = System.Drawing.Color.White;
+            beteven.Location = new System.Drawing.Point(563, 366);
+            beteven.Name = "beteven";
+            beteven.Size = new System.Drawing.Size(94, 29);
+            beteven.TabIndex = 7;
+            beteven.Text = "Bet Even";
+            beteven.UseVisualStyleBackColor = false;
+            // 
+            // reset
+            // 
+            reset.BackColor = System.Drawing.Color.Gray;
+            reset.ForeColor = System.Drawing.Color.White;
+            reset.Location = new System.Drawing.Point(672, 61);
+            reset.Name = "reset";
+            reset.Size = new System.Drawing.Size(94, 29);
+            reset.TabIndex = 8;
+            reset.Text = "Reset Game";
+            reset.UseVisualStyleBackColor = false;
+            // 
+            // resultat
+            // 
+            resultat.AutoSize = true;
+            resultat.ForeColor = System.Drawing.Color.Yellow;
+            resultat.Location = new System.Drawing.Point(384, 61);
+            resultat.Name = "resultat";
+            resultat.Size = new System.Drawing.Size(0, 20);
+            resultat.TabIndex = 9;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(330, 370);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(169, 20);
+            label2.TabIndex = 10;
+            label2.Text = "Choose a number (0-36)";
+            // 
+            // nrbtn
+            // 
+            nrbtn.Location = new System.Drawing.Point(347, 403);
+            nrbtn.Name = "nrbtn";
+            nrbtn.Size = new System.Drawing.Size(125, 27);
+            nrbtn.TabIndex = 11;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new System.Drawing.Point(805, 341);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new System.Drawing.Size(8, 8);
+            pictureBox2.TabIndex = 12;
+            pictureBox2.TabStop = false;
+            // 
+            // betbtn
+            // 
+            betbtn.BackColor = System.Drawing.SystemColors.GrayText;
+            betbtn.ForeColor = System.Drawing.Color.White;
+            betbtn.Location = new System.Drawing.Point(152, 61);
+            betbtn.Name = "betbtn";
+            betbtn.Size = new System.Drawing.Size(94, 29);
+            betbtn.TabIndex = 13;
+            betbtn.Text = "Bet";
+            betbtn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.DarkGreen;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(betbtn);
+            Controls.Add(pictureBox2);
+            Controls.Add(nrbtn);
+            Controls.Add(label2);
+            Controls.Add(resultat);
+            Controls.Add(reset);
+            Controls.Add(beteven);
+            Controls.Add(betodd);
+            Controls.Add(betblack);
+            Controls.Add(betred);
+            Controls.Add(label1);
+            Controls.Add(betamount);
+            Controls.Add(balance);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Roulette Game";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label balance;
+        private System.Windows.Forms.TextBox betamount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button betred;
+        private System.Windows.Forms.Button betblack;
+        private System.Windows.Forms.Button betodd;
+        private System.Windows.Forms.Button beteven;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Label resultat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nrbtn;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button betbtn;
+    }
+}
---
+++ Gui roulette/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Gui_roulette
+{
+    public partial class Form1 : Form
+    {
+        private decimal playerBalance = 100;
+        private string selectedBetType = null; // "Red", "Black", "Even", "Odd" eller null
+        private readonly HashSet<int> redNumbers = new HashSet<int> { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+        private readonly Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateBalanceDisplay();
+
+            // Koppla eventhanterare
+            betred.Click += (s, e) => { selectedBetType = "Red"; resultat.Text = "Vald: Rött"; UpdateButtonColors(); };
+            betblack.Click += (s, e) => { selectedBetType = "Black"; resultat.Text = "Vald: Svart"; UpdateButtonColors(); };
+            beteven.Click += (s, e) => { selectedBetType = "Even"; resultat.Text = "Vald: Jämnt"; UpdateButtonColors(); };
+            betodd.Click += (s, e) => { selectedBetType = "Odd"; resultat.Text = "Vald: Udda"; UpdateButtonColors(); };
+            betbtn.Click += BetBtn_Click;
+            reset.Click += Reset_Click;
+
+            // Endast siffror i textfält
+            betamount.KeyPress += (s, e) => { if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) e.Handled = true; };
+            nrbtn.KeyPress += (s, e) => { if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) e.Handled = true; };
+        }
+
+        private void UpdateBalanceDisplay()
+        {
+            balance.Text = $"Balance: ${playerBalance}";
+        }
+
+        private void UpdateButtonColors()
+        {
+            betred.BackColor = selectedBetType == "Red" ? Color.DarkRed : Color.Red;
+            betblack.BackColor = selectedBetType == "Black" ? Color.DarkGray : Color.Black;
+            beteven.BackColor = selectedBetType == "Even" ? Color.BurlyWood : Color.SandyBrown;
+            betodd.BackColor = selectedBetType == "Odd" ? Color.DimGray : Color.Gray;
+        }
+
+        private void BetBtn_Click(object sender, EventArgs e)
+        {
+            // 1. Validera insats
+            if (string.IsNullOrWhiteSpace(betamount.Text))
+            {
+                resultat.Text = "❌ Ange insatsbelopp!";
+                return;
+            }
+
+            if (!decimal.TryParse(betamount.Text, out decimal betAmount) || betAmount <= 0)
+            {
+                resultat.Text = "❌ Ogiltig insats!";
+                return;
+            }
+
+            if (betAmount > playerBalance)
+            {
+                resultat.Text = $"❌ Du har bara ${playerBalance} kvar!";
+                return;
+            }
+
+            // 2. Hämta valt nummer (om angivet)
+            bool hasNumber = !string.IsNullOrWhiteSpace(nrbtn.Text);
+            int selectedNumber = -1;
+
+            if (hasNumber)
+            {
+                if (!int.TryParse(nrbtn.Text, out selectedNumber) || selectedNumber < 0 || selectedNumber > 36)
+                {
+                    resultat.Text = "❌ Ogiltigt nummer (0-36)!";
+                    return;
+                }
+            }
+
+            // 3. Validera kombination OM både bettyp och nummer är valda
+            if (!string.IsNullOrEmpty(selectedBetType) && hasNumber)
+            {
+                if (selectedNumber == 0)
+                {
+                    resultat.Text = "❌ 0 är GRÖNT! Kan ej kombineras med Rött/Svart/Jämnt/Udda.";
+                    return;
+                }
+
+                if (selectedBetType == "Red" && !redNumbers.Contains(selectedNumber))
+                {
+                    resultat.Text = $"❌ {selectedNumber} är SVART, inte rött!";
+                    return;
+                }
+
+                if (selectedBetType == "Black" && redNumbers.Contains(selectedNumber))
+                {
+                    resultat.Text = $"❌ {selectedNumber} är RÖTT, inte svart!";
+                    return;
+                }
+
+                if (selectedBetType == "Even" && selectedNumber % 2 != 0)
+                {
+                    resultat.Text = $"❌ {selectedNumber} är UDDA, inte jämnt!";
+                    return;
+                }
+
+                if (selectedBetType == "Odd" && selectedNumber % 2 == 0)
+                {
+                    resultat.Text = $"❌ {selectedNumber} är JÄMNT, inte udda!";
+                    return;
+                }
+            }
+
+            // 4. Generera slumpresultat (0-36)
+            int resultNumber = random.Next(0, 37);
+            string resultColor = GetColor(resultNumber);
+            bool isEven = (resultNumber != 0 && resultNumber % 2 == 0);
+
+            // 5. Beräkna vinst/förlust - ENLIGT DINA REGLER:
+            bool win = false;
+            decimal payout = 0;
+
+            // FALL 1: Satsa ENBART på nummer (ingen färg/typ vald)
+            if (string.IsNullOrEmpty(selectedBetType) && hasNumber)
+            {
+                if (resultNumber == selectedNumber)
+                {
+                    win = true;
+                    payout = selectedNumber == 0 ? betAmount * 100 : betAmount * 2; // 100x för 0, 2x för andra nummer
+                }
+                // Annars förlust (win = false)
+            }
+            // FALL 2: Satsa på färg/typ + nummer
+            else if (!string.IsNullOrEmpty(selectedBetType) && hasNumber)
+            {
+                if (resultNumber == 0)
+                {
+                    // 0 = alltid förlust för färg/typ-satsningar
+                    win = false;
+                }
+                else if (selectedBetType == "Red" && resultColor == "Red")
+                {
+                    if (resultNumber == selectedNumber)
+                        payout = betAmount * 4; // Både färg och nummer rätt
+                    else
+                        payout = betAmount * 2; // Endast färg rätt
+                    win = true;
+                }
+                else if (selectedBetType == "Black" && resultColor == "Black")
+                {
+                    if (resultNumber == selectedNumber)
+                        payout = betAmount * 4;
+                    else
+                        payout = betAmount * 2;
+                    win = true;
+                }
+                else if (selectedBetType == "Even" && isEven)
+                {
+                    if (resultNumber == selectedNumber)
+                        payout = betAmount * 4;
+                    else
+                        payout = betAmount * 2;
+                    win = true;
+                }
+                else if (selectedBetType == "Odd" && !isEven)
+                {
+                    if (resultNumber == selectedNumber)
+                        payout = betAmount * 4;
+                    else
+                        payout = betAmount * 2;
+                    win = true;
+                }
+                // Annars förlust
+            }
+            // FALL 3: Satsa ENBART på färg/typ (inget nummer)
+            else if (!string.IsNullOrEmpty(selectedBetType) && !hasNumber)
+            {
+                if (resultNumber == 0)
+                {
+                    // 0 = alltid förlust för färg/typ-satsningar
+                    win = false;
+                }
+                else if (selectedBetType == "Red" && resultColor == "Red")
+                {
+                    win = true;
+                    payout = betAmount * 2;
+                }
+                else if (selectedBetType == "Black" && resultColor == "Black")
+                {
+                    win = true;
+                    payout = betAmount * 2;
+                }
+                else if (selectedBetType == "Even" && isEven)
+                {
+                    win = true;
+                    payout = betAmount * 2;
+                }
+                else if (selectedBetType == "Odd" && !isEven)
+                {
+                    win = true;
+                    payout = betAmount * 2;
+                }
+                // Annars förlust
+            }
+            // FALL 4: Ingen satsning gjord
+            else
+            {
+                resultat.Text = "❌ Välj antingen en färg/typ ELLER ett nummer att satsa på!";
+                return;
+            }
+
+            // 6. Visa resultat och uppdatera saldo
+            if (win)
+            {
+                playerBalance += payout - betAmount;
+                resultat.Text = $"🎉 VINST! +${payout} | Resultat: {resultNumber} ({resultColor})";
+            }
+            else
+            {
+                playerBalance -= betAmount;
+                resultat.Text = $"❌ FÖRLUST -${betAmount} | Resultat: {resultNumber} ({resultColor})" +
+                               (resultNumber == 0 ? " - 0 är GRÖNT!" : "");
+            }
+
+            UpdateBalanceDisplay();
+            selectedBetType = null;
+            UpdateButtonColors();
+
+            // 7. Game over?
+            if (playerBalance <= 0)
+            {
+                resultat.Text = "💰 GAME OVER! Tryck på Reset för att börja om.";
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            playerBalance = 100;
+            selectedBetType = null;
+            betamount.Text = "";
+            nrbtn.Text = "";
+            resultat.Text = "✨ Spelet återställt - du har $100 att spela med!";
+            UpdateBalanceDisplay();
+            UpdateButtonColors();
+        }
+
+        private string GetColor(int number)
+        {
+            if (number == 0) return "Green";
+            return redNumbers.Contains(number) ? "Red" : "Black";
+        }
+    }
+}
